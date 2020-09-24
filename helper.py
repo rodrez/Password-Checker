e2e250ad3d32a92834108cbc1e94d9509c841234
Rodrez <fabian.rodrez@gmail.com>
2020-09-23 21:33:44 -0400

    Started new gui using KivyMD

---
+++ helper.py
@@ -0,0 +1,23 @@
+username_helper = """
+MDTextField:
+    hint_text: "Enter Username"
+    helper_text: "Forgot username? Reset"
+    helper_text_mode: "on_focus"
+    icon_right: "chevron-right"
+    icon_right_color: app.theme_cls.primary_color
+    pos_hint: {'center_x':0.5, 'center_y':0.5}
+    size_hint_x: None
+    width: 600
+"""
+
+password_helper = """
+MDTextField:
+    hint_text: "Enter Password"
+    helper_text: "Forgot password? Reset"
+    helper_text_mode: "on_focus"
+    icon_right: "chevron-right"
+    icon_right_color: app.theme_cls.primary_color
+    pos_hint: {'center_x':0.5, 'center_y':0.4}
+    size_hint_x: None
+    width: 600
+"""
--- main.py
+++ main.py
@@ -1,24 +1,50 @@
-import sqlite3
+from kivymd.app import MDApp
+# from kivymd.uix.label import MDLabel, MDIcon
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.button import MDFlatButton, MDRectangleFlatButton, MDRaisedButton
+from kivy.lang import Builder
+from helper import username_helper, password_helper
+from kivy.core.window import Window
 
-conn = sqlite3.connect('PasswordManager.db')
 
-print("Opened database successfully!")
+Window.size = (500, 800)
 
-# conn.execute('''CREATE TABLE PASSWORDS
-#          (ID INT PRIMARY KEY     NOT NULL,
-#          NAME           TEXT    NOT NULL,
-#          LOGIN          TEXT    NOT NULL,
-#          PASSWORD       TEXT     NOT NULL);''')
-# print("Table created successfully");
-conn.execute("INSERT INTO PASSWORDS (ID,NAME,LOGIN,PASSWORD) \
-      VALUES (2, 'gmail', 'fabian.rodrez@gmail.com', '@Monarch6419' )");
-conn.commit()
-cursor = conn.execute("SELECT id, name, login, password from PASSWORDS")
-for row in cursor:
-    print("ID", row[0])
-    print("NAME", row[1])
-    print("LOGIN", row[2])
-    print("PASSWORD", row[3])
-    
 
-conn.close()
+class DemoApp(MDApp):
+
+    # def build(self):
+    #     self.theme_cls.primary_palette = "Yellow"
+    #     self.theme_cls.primary_hue = "700"
+    #     self.theme_cls.theme_style = "Dark"
+    #     # label = MDLabel(text="Password Manager", halign='center', theme_text_color='Custom',
+    #     #                 text_color=(108 / 255.0, 14 / 255.0, 232 / 255.0, 1),
+    #     #                 font_style='H3')
+    #     # icon_label = MDIcon(icon='language-python', halign="center")
+    #     screen = MDScreen()
+    #     btn_flat = MDRectangleFlatButton(text='Sign Up', pos_hint={'center_x':0.5, 'center_y':0.5})
+    #     # icon_btn = MDFloatingActionButton(icon="android", pos_hint={'center_x':0.5, 'center_y':0.5})
+    #     # float_btn = MDFloatingActionButton()
+    #     screen.add_widget(btn_flat)
+    #     return screen
+
+    def build(self):
+        self.theme_cls.primary_palette = "Indigo"
+        self.theme_cls.primary_hue = "A700"
+        self.theme_cls.theme_style = "Light"
+        screen = MDScreen()
+        # username = MDTextField(text="Enter Username", pos_hint={"center_x": 0.5, "center_y": 0.5}, size_hint_x=None, width=400)
+        user_button = MDRaisedButton(text="Search", pos_hint={'center_x':0.5, 'center_y':0.3},
+                                       on_release=self.show_data)
+        self.username = Builder.load_string(username_helper)
+        self.password = Builder.load_string(password_helper)
+        screen.add_widget(self.username)
+        screen.add_widget(self.password)
+        screen.add_widget(user_button)
+        return screen
+
+    def show_data(self, obj):
+        print(self.username.text)
+
+
+if __name__ == '__main__':
+    DemoApp().run()
